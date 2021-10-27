1aaa8ba42b4b3c11e9a5eb9e310df29aa28d3257
Kurbangaliev Renat <kurbangaliev1987@mail.ru>
2021-10-27 16:11:02 +0300

    third step

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,8 @@
-//popup
-
 let popupElement = document.querySelector('.popup');
 let formElement = popupElement.querySelector('.popup__submit-form')
-let nameInput = popupElement.querySelector('.popup__input_name');
-let jobInput = popupElement.querySelector('.popup__input_job');
+let nameInput = popupElement.querySelector('.popup__input_type_name');
+let jobInput = popupElement.querySelector('.popup__input_type_job');
 let popupCloseButtonElement = popupElement.querySelector('.popup__close')
-
-//page
 
 let profileElement = document.querySelector('.profile');
 let profileTitle = profileElement.querySelector('.profile__title');
@@ -14,16 +10,14 @@ let profileSubtitle = profileElement.querySelector('.profile__subtitle');
 let profileEditButton = profileElement.querySelector('.profile__edit-button');
 let profileAddButton = profileElement.querySelector('.profile__add-button');
 
-//передача данных в форму popup
-
 const popupOpen = function() {
-  popupElement.classList.add('popup_condition_opened');
+  popupElement.classList.add('popup_is-opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
 const popupClose = function() {
-  popupElement.classList.remove('popup_condition_opened');
+  popupElement.classList.remove('popup_is-opened');
 }
 
 profileEditButton.addEventListener('click', popupOpen);
