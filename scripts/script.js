4946d8564850bb57d74a8276879ad8f3e5ec7062
Kurbangaliev Renat <kurbangaliev1987@mail.ru>
2021-11-01 14:12:42 +0300

    feature auto loading cards

--- scripts/script.js
+++ scripts/script.js
@@ -32,3 +32,61 @@ let formSubmitHandler = function(evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+//sprint 5
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsTemplate = document.querySelector('#card').content;
+
+const cards = document.querySelector('.cards');
+
+//добавление карточек при загрузке
+
+initialCards.forEach(el => {
+
+const cardsElement = cardsTemplate.querySelector('.card').cloneNode(true);
+
+cardsElement.querySelector('.card__photo').src = el.link;
+
+cardsElement.querySelector('.card__title').textContent = el.name;
+
+cards.appendChild(cardsElement);
+})
+
+
+
+
+
+
+
+
+
+
+
+
