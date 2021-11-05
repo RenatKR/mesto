7abb06df2c9ba4a7b0852554217d09f29f5481bb
RenatKR <kurbangaliev1987@mail.ru>
2021-11-05 18:16:18 +0300

    feature like popup-img

--- scripts/script.js
+++ scripts/script.js
@@ -35,8 +35,7 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 //sprint 5
 
-const initialCards = [
-  {
+const initialCards = [{
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
@@ -70,13 +69,23 @@ const cards = document.querySelector('.cards');
 
 initialCards.forEach(el => {
 
-const cardsElement = cardsTemplate.querySelector('.card').cloneNode(true);
+  const cardsElement = cardsTemplate.querySelector('.card').cloneNode(true);
 
-cardsElement.querySelector('.card__photo').src = el.link;
+  cardsElement.querySelector('.card__photo').src = el.link;
 
-cardsElement.querySelector('.card__title').textContent = el.name;
+  cardsElement.querySelector('.card__title').textContent = el.name;
 
-cards.appendChild(cardsElement);
+  cardsElement.querySelector('.card__like').addEventListener('click', function(evt) { evt.target.classList.toggle('card__like_active') });
+
+  cardsElement.querySelector('.card__trash').addEventListener('click', function(evt) { evt.target.parentElement.remove(); })
+
+  cardsElement.querySelector('.card__photo').addEventListener('click', function() {
+    popupImg.classList.add('popup_is-opened');
+    popupImgImg.src = el.link;
+    popupImgTitle.textContent = el.name;
+  })
+
+  cards.appendChild(cardsElement);
 })
 
 //добавление и удаление popupAdd
@@ -88,50 +97,46 @@ let placeInput = popupAddElement.querySelector('.popup__input_type_place');
 let srcInput = popupAddElement.querySelector('.popup__input_type_src');
 let profileCloseAddButton = popupAddElement.querySelector('.popup__close');
 
-const popupAddOpen = function() {
-  popupAddElement.classList.add('popup_is-opened');
-}
 
-profileAddButton.addEventListener('click', popupAddOpen);
+profileAddButton.addEventListener('click', function() {
+  popupAddElement.classList.add('popup_is-opened');
+});
 
 const popupAddClose = function() {
   popupAddElement.classList.remove('popup_is-opened');
-} 
+}
 
 profileCloseAddButton.addEventListener('click', popupAddClose);
 
 //добавление карточек
 
 let formAddSubmitHandler = function(evt) {
-evt.preventDefault();
-const cardAddElement = cardsTemplate.querySelector('.card').cloneNode(true);
-cardAddElement.querySelector('.card__photo').src = srcInput.value;
-cardAddElement.querySelector('.card__title').textContent = placeInput.value;
-cards.prepend(cardAddElement);
-popupAddClose();
+  evt.preventDefault();
+  const cardAddElement = cardsTemplate.querySelector('.card').cloneNode(true);
+  cardAddElement.querySelector('.card__photo').src = srcInput.value;
+  cardAddElement.querySelector('.card__title').textContent = placeInput.value;
+  cardAddElement.querySelector('.card__like').addEventListener('click', function(evt) { evt.target.classList.toggle('card__like_active') });
+  cardAddElement.querySelector('.card__trash').addEventListener('click', function(evt) { evt.target.parentElement.remove(); })
+  cardAddElement.querySelector('.card__photo').addEventListener('click', function() {
+    popupImg.classList.add('popup_is-opened');
+    popupImgImg.src = srcInput.value;
+    popupImgTitle.textContent = placeInput.value;
+  })
+  cards.prepend(cardAddElement);
+  popupAddClose();
 }
 
 formAddElement.addEventListener('submit', formAddSubmitHandler);
 
-//лайк карточки
+// открытие popup-image с картинкой
 
-//const likeButton = document.querySelectorAll('.card__like');
+let popupImg = document.querySelector('.popup-image')
+let cardTitle = document.querySelector('.card__title')
+let popupImgImg = popupImg.querySelector('.popup-image__img');
+let popupImgTitle = popupImg.querySelector('.popup-image__title');
 
-//console.log(likeButton);
-//const likeImg = document.querySelectorAll('.card__like-img');
-//console.log(likeImg.src);
+let popupImgButtonClose = popupImg.querySelector('.popup-image__close');
 
-//const AddLike = function () {
- // LikeImg.src = 
-//} 
-
-// удаление карточки
-
-let delButton = document.querySelectorAll('.card__trash');
-
-delButton.forEach (function(el) {
-  const closestCard = el.parentElement;
-  el.addEventListener('click', function () {
-    closestCard.remove();
-  })
+popupImgButtonClose.addEventListener('click', function() {
+  popupImg.classList.remove('popup_is-opened');
 });
